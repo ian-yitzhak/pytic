9f5f49a976dbbc8026b0ae7fd48081af1235b42c
ian-yitzhak <61430847+ian-yitzhak@users.noreply.github.com>
2020-09-10 18:09:19 +0300

    Add files via upload

---
+++ tic.py
@@ -0,0 +1,10 @@
+board = ["_","_","_",
+          "_","_","_",
+           "_","_","_"]
+
+def display_board():
+        print(board[0] +"|" + board[1] + "|" +board[2] )
+        print(board[4] +"|" + board[5] + "|" +board[6] )
+        print(board[7] +"|" + board[8] + "|" +board[9] )
+
+display_board()
